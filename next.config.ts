17bfcaa718b893db4ef88e6f88f6224f010ffa6e
armanh20439 <armanh20439@gmail.com>
2025-10-30 12:29:23 +0600

    2nd

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,36 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'tse3.mm.bing.net',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'scitechdaily.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'th.bing.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'media.gettyimages.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'media.licdn.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
--- src/components/shared/NewCard.tsx
+++ src/components/shared/NewCard.tsx
@@ -1,29 +1,38 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
 
 import { Button } from '../ui/button'
-import {NewCardProps} from "@/types/news"
-const newsImage = ({item}:NewCardProps) => {
+import { NewCardProps } from "@/types/news"
+
+const NewsImage = ({ item }: NewCardProps) => {
   return (
-    <div>
-       <div className='border-4 p-4 rounded-md shadow-emerald-500'>
-            <Link href={`news/${item?._id}`}>
-                <Image src={item?.imageUrl} width={500} height={500} alt='new Cart'/>            
-            </Link>
-            <h2 className='text-xl font-semibold my-3'>
-                {item.title}
-            </h2>
-            <p className='mb-4'>
-                {item.description}
-            </p>
-           
-            <Link href={`news/${item?._id}`}>
-                <Button variant="default">Read More</Button>
-            </Link>
-       </div>
+    <div className="h-full">
+      <div className="border-4 p-4 rounded-md shadow-emerald-500 h-full flex flex-col">
+        <Link href={`news/${item?._id}`}>
+          <Image
+            src={item?.imageUrl}
+            width={500}
+            height={500}
+            alt="news card"
+            priority
+            className="w-full h-[200px] object-cover rounded"
+          />
+        </Link>
+
+        <h2 className="text-xl font-semibold my-3">
+          {item.title}
+        </h2>
+
+        <p className="text-gray-600 line-clamp-3 flex-grow">
+          {item.description}
+        </p>
+
+        <Link href={`news/${item?._id}`}>
+          <Button variant="default" className="mt-4">Read More</Button>
+        </Link>
+      </div>
     </div>
   )
 }
 
-export default newsImage
+export default NewsImage
